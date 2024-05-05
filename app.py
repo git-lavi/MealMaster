cbb0af56468da5db34d13a96d6ec2391c4da36a4
git-lavi <lavitra07dahal@gmail.com>
2024-05-05 16:51:23 +0545

    functionality to update meal and food tables

--- app.py
+++ app.py
@@ -150,21 +150,33 @@ def dashboard():
 @login_required
 def diary():
     current_user = session['username']
-    meals = []
 
     try:
         conn, cursor = connect_to_db()
-        cursor.execute("SELECT meal_name from meals WHERE username  = ?", (session['username'],))
+        cursor.execute("SELECT * from meals WHERE username  = ?", (session['username'],))
         records = cursor.fetchall()
-        for record in records:
-            meals.append(record[0])
+        
+        # Convert records to a list of dictionaries
+        meal_columns = [column[0] for column in cursor.description]
+        meals = [dict(zip(meal_columns, record)) for record in records]
+        
+
+        # Fetch foods for each meal
+        for meal in meals:
+            cursor.execute("SELECT * FROM foods WHERE meal_id = ?", (meal['meal_id'],))
+            food_records = cursor.fetchall()
+            food_columns = [column[0] for column in cursor.description]
+            meal['foods'] = [dict(zip(food_columns, record)) for record in food_records]
+
+        # testprint
+        print(meal_columns, meals)
+
     except sqlite3.Error as e:
         flash("Error fetching meals")
         print("Error in db while fetching meals -", e)
 
     finally:
         conn.close()
-
     return render_template("diary.html", current_user=current_user, meals=meals)
 
 
@@ -207,38 +219,62 @@ def add_meal():
 @app.route("/add_food", methods=['POST'])
 @login_required
 def add_food():
-    meal_name = request.form.get('meal-name')
+    meal_id = request.form.get('meal-id')
     food_name = request.form.get("food-name")
     food_servings = request.form.get("food-servings")
 
-    if not meal_name or not food_name or not food_servings or not food_servings.isdigit():
+    if not meal_id or not food_name or not food_servings or not food_servings.isdigit():
         flash("invalid input", "error")
         return redirect("/diary")
     
     food_servings = int(food_servings)
 
     try:
-        nix_item_id = get_food_nix_id(food_name)
+        nix_food_name = get_nix_food_name(food_name)
     except Exception as e:
         flash(str(e), "error")
         return redirect("/diary")
     
-    nutrients = get_nutrients(nix_item_id, food_servings)
-    
+    nutrients = get_nutrients(nix_food_name, food_servings)
+
     if not nutrients:
         flash("Error fetching nutrients", "error")
         return redirect("/diary")
     
-    print(f"meal_name: {meal_name}, food_name: {food_name}, food_servings: {food_servings}, nix_item_id: {nix_item_id}, nutrients: {nutrients}")
+    # Nutrient details from nutrients
+    calories, protein, carbohydrates, fat = nutrients['calories'], nutrients['protein'], nutrients['carbohydrates'], nutrients['fat']
     
+    print(f"meal_id: {meal_id}, food_name: {food_name}, food_servings: {food_servings}, nix_food_name: {nix_food_name}, calories: {calories}, protein: {protein}, carbohydrates: {carbohydrates}, fat: {fat}")
+
+    # Insert food into DB
+    try:
+        conn, cursor = connect_to_db()
+        cursor.execute("""
+                       INSERT INTO foods (food_name, servings, calories, protein, carbohydrates, fat, meal_id)
+                       VALUES (?, ?, ?, ?, ?, ?, ?)
+                       """, (food_name, food_servings, calories, protein, carbohydrates, fat, meal_id)
+                       )
+    except sqlite3.Error as e:
+        print("Error entering food into DB", e)
+        flash("An error occurred while adding food", "error")
+        conn.rollback()
+    finally:
+        if conn:
+            conn.commit()
+            conn.close()
+        
+    flash("Food added successfully", "success")
     return redirect("/diary")
 
 
-def get_food_nix_id(food_name):
+def get_nix_food_name(food_name):
     # Call API
     url = 'https://trackapi.nutritionix.com/v2/search/instant'
     headers = {'Content-Type': 'application/x-www-form-urlencoded', 'x-app-id': APP_ID, 'x-app-key': API_KEY}
     params = {'query': food_name}
+    
+    # testprint
+    print(params)
     
     response = requests.get(url, headers=headers, params=params)
     print("get_food_nix_id API call response:",  response)
@@ -248,49 +284,40 @@ def get_food_nix_id(food_name):
     
     data = response.json()
     
-    # Get food only from branded item list
-    branded_list = data.get('branded', [])
-    if not branded_list:
-        print("Error obtaining branded food from Nutritionix API")
-        raise NixAPICallError("Sorry, we couldn't find this food.")
-    nix_item_id = branded_list[0]['nix_item_id']
-
-    return nix_item_id
-
-
-def get_nutrients(nix_item_id: str, servings: int):
-    url = 'https://trackapi.nutritionix.com//v2/search/item'
-    headers = {'Content-Type': 'application/x-www-form-urlencoded', 'x-app-id': APP_ID, 'x-app-key': API_KEY}
-    params = {'nix_item_id': nix_item_id}
+    # Get food only from common item list
+    common_list = data.get('common', [])
     
-    response = requests.get(url, headers=headers, params=params)
-    print("get_nutrients API call response:", response)
+    if not common_list:
+        print("Error obtaining food from Nutritionix API")
+        raise NixAPICallError("Sorry, we couldn't find this food.")
+    
+    nix_food_name = common_list[0]['food_name']
+    return nix_food_name      
 
-    if response.status_code != 200:
-        print("Failed to fetch nutrient data from Nutritionix API")
-        return None
 
+def get_nutrients(nix_food_name: str, servings: int):
+    url = 'https://trackapi.nutritionix.com/v2/natural/nutrients'
+    headers = {'Content-Type': 'application/json', 'x-app-id': APP_ID, 'x-app-key': API_KEY}
+    json = {'query': nix_food_name}
+
+    response = requests.post(url, headers=headers, json=json)
+    print("get_nutrition API call response:", response)
     data = response.json()
 
-    if 'foods' not in data or not data['foods']:
-        return None
+    foods = data['foods']
+    nf_calories = foods[0]['nf_calories']
+    nf_protein = foods[0]['nf_protein']
+    nf_total_carbohydrate = foods[0]['nf_total_carbohydrate']
+    nf_total_fat = foods[0]['nf_total_fat']
 
-    # Getting necessary values from Nutritionix
-    serving_qty = data['foods'][0]['serving_qty']
-    nf_calories = data['foods'][0]['nf_calories']
-    nf_protein = data['foods'][0]['nf_protein']
-    nf_total_carbohydrate = data['foods'][0]['nf_total_carbohydrate']
-    nf_total_fat = data['foods'][0]['nf_total_fat']
+    # Multiply with servings
+    calories = int(round((nf_calories * servings), 0))
+    protein = int(round((nf_protein * servings), 0))
+    carbohydrates = int(round((nf_total_carbohydrate * servings), 0))
+    fat = int(round((nf_total_fat * servings), 0))
 
-    print(f"nf_calories: {type(nf_calories)}, serving_qty: {type(serving_qty)}, servings: {type(servings)}")
-
-    calories = int(round((nf_calories / serving_qty * servings), 2))
-    protein = int(round((nf_protein / serving_qty * servings), 2))
-    carbohydrates = int(round((nf_total_carbohydrate / serving_qty * servings), 2))
-    fat = int(round((nf_total_fat / serving_qty * servings), 2))
-
-    return {'calories': calories, 'protein': protein, 'carbohydates': carbohydrates, 'fat': fat, 'serving_qty': serving_qty, 'servings': servings}
-        
+    print(f"calories = {calories}, protein = {protein}, carbohydrates = {carbohydrates}, fat = {fat}")
+    return {'calories': calories, 'protein': protein, 'carbohydrates': carbohydrates, 'fat': fat, 'servings': servings}
 
 
 @app.route("/logout")
